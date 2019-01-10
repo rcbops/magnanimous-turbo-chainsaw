a59a1a9837291bbb7eccc73d60150caec35de9b8
Kevin Carter <kevin@cloudnull.com>
2019-01-10 14:06:42 -0600

    separate elk cleanup and filebeat
    Signed-off-by: Kevin Carter <kevin@cloudnull.com>

--- scripts/cleanup-legacy-filebeat.sh
+++ scripts/cleanup-legacy-filebeat.sh
@@ -7,4 +7,4 @@ source "$(dirname $(readlink -f ${BASH_SOURCE[0]}))/set-vars.sh"
 source "$(dirname $(readlink -f ${BASH_SOURCE[0]}))/setup-workspace.sh"
 
 # Cleanup all legacy systems prior to deployment
-ansible-playbook ${ANSIBLE_EXTRA_VARS:-} "/opt/magnanimous-turbo-chainsaw/playbooks/cleanup-legacy-filebeat.yml"
+ansible-playbook ${ANSIBLE_EXTRA_VARS:-} "${PLAYBOOK_DIR}/cleanup-legacy-filebeat.yml"
