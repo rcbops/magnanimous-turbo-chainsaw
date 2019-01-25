424e61f1dabbd7fb0f28798a65d947de81211136
Kevin Carter <kevin@cloudnull.com>
2019-01-25 13:39:57 -0600

    add TEST_TYPE variable
    Signed-off-by: Kevin Carter <kevin@cloudnull.com>

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,32 +1,24 @@
 #!/usr/bin/env bash
-# Copyright 2018, Rackspace US, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 set -ve
 
+## Variables -----------------------------------------------------------------
 export TEST_DIR="$(readlink -f $(dirname ${0})/../)"
+export TEST_TYPE="${TEST_TYPE:-aio}"
 
+## Main ----------------------------------------------------------------------
 bash ${TEST_DIR}/scripts/setup.sh
 
-${HOME}/ansible_venv/bin/ansible-playbook -i 'localhost,' \
-                                          -vv \
-                                          -e ansible_connection=local \
-                                          ${TEST_DIR}/playbooks/test-basic-setup.yml
+if [[ "${TEST_TYPE}" == "aio" ]]; then
+    eval "${HOME}/ansible_venv/bin/ansible-playbook" -i 'localhost,' \
+                                                     -vv \
+                                                     -e ansible_connection=local \
+                                                     ${TEST_DIR}/playbooks/test-basic-setup.yml
 
-${HOME}/ansible_venv/bin/ansible-playbook -i 'localhost,' \
-                                          -vv \
-                                          -e ansible_connection=local \
-                                          ${TEST_DIR}/tests/playbooks/test.yml
+    eval "${HOME}/ansible_venv/bin/ansible-playbook" -i 'localhost,' \
+                                                     -vv \
+                                                     -e ansible_connection=local \
+                                                     ${TEST_DIR}/tests/playbooks/test.yml
 
-source ${TEST_DIR}/scripts/deploy-all.sh
+    source ${TEST_DIR}/scripts/deploy-all.sh
+fi
